6a8261ed0b5d33b18cf6ed71c5b41afc3a9c7b18
unknown <nandasyarla58911@gmail.com>
2023-03-14 17:55:26 +0700

    Tampilkan table customers (v.1)

--- index.php
+++ index.php
@@ -5,6 +5,46 @@
 <!DOCTYPE html>
 <html>
      <body>
+     <table>
+            <tr>
+
+                <th> customerNumber </th>
+                <th> customerName</th>
+                <th> contactLastName</th>
+                <th> contactFirstName</th>
+                <th> phone</th>
+                <th> adressLine1</th>
+                <th> adressLine2</th>
+                <th> city</th>
+                <th> state</th>
+                <th> postalCode</th>
+                <th> country</th>
+                <th> salesRepEmployeeNumber</th>
+                <th> creditLimit</th>
+            </tr>
+            <?php
+            $sql = "SELECT * FROM customers";
+            $result = mysqli_query($conn, $sql);
+            while($row = mysqli_fetch_assoc($result)) {
+                echo "<tr>";
+                echo "<td>" .$row["customerNumber"] . "</td>";
+                echo "<td>" .$row["customerName"] . "</td>";
+                echo "<td>" .$row["contactLastName"] . "</td>";
+                echo "<td>" .$row["contactFirstName"] . "</td>";
+                echo "<td>" .$row["phone"] . "</td>";
+                echo "<td>" .$row["adressLine1"] . "</td>";
+                echo "<td>" .$row["adressLine2"] . "</td>";
+                echo "<td>" .$row["city"] . "</td>";
+                echo "<td>" .$row["state"] . "</td>";
+                echo "<td>" .$row["postalCode"] . "</td>";
+                echo "<td>" .$row["country"] . "</td>";
+                echo "<td>" .$row["salesRepEmployeeNumber"] . "</td>";
+                echo "<td>" .$row["creditLimit"] . "</td>";
+                echo "</tr>";
+            }
+            ?>
+        </table> 
+
         <table>
             <tr>
                 <th> productCode </th>
